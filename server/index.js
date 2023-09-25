49579bb315977cbef5bc325a8517b73b35c137d3
Alan <zeyu.weng@gmail.com>
2023-09-25 19:50:36 -0400

    no longer using external libaries to make http requests

---
+++ server/index.js
@@ -0,0 +1,72 @@
+const test = require('dotenv').config()
+const express = require("express");
+const querystring = require("node:querystring");
+const crypto = require("crypto");
+
+const port = 3000;
+const CLIENT_ID = process.env.SPOTIFY_CLIENT_ID;
+const CLIENT_SECRET = process.env.SPOTIFY_CLIENT_SECRET;
+const redirect_uri = "http://localhost:3000/callback";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello world!");
+});
+
+app.get("/login", (req, res) => {
+  let state = crypto.randomBytes(16).toString("hex");
+  let scope = "user-read-private user-read-email";
+  res.redirect(
+    "https://accounts.spotify.com/authorize?" +
+    querystring.stringify({
+      response_type: "code",
+      client_id: CLIENT_ID,
+      scope: scope,
+      redirect_uri: redirect_uri,
+      state: state,
+    })
+  );
+});
+
+app.get("/callback", async (req, res) => {
+  let code = req.query.code;
+  let state = req.query.state;
+  let authOptions = {
+    url: "https://accounts.spotify.com/api/token",
+    form: {
+      code: code,
+      redirect_uri: redirect_uri,
+      grant_type: "authorization_code",
+    },
+    headers: {
+      "Authorization":
+        "Basic " +
+        new Buffer.from(CLIENT_ID + ":" + CLIENT_SECRET).toString("base64"),
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    json: true,
+  };
+
+
+  const response = await fetch(authOptions.url, { headers: authOptions.headers, method: "POST", body: new URLSearchParams(authOptions.form) })
+  const result = await response.json()
+  const access_token = result.access_token
+  const refresh_token = result.refresh_token
+
+  if (access_token) {
+    const data = await fetch("https://api.spotify.com/v1/me", {
+      headers: {
+        Authorization: `Bearer ${access_token}`,
+      }
+    })
+    const profile = await data.json()
+    console.log(profile)
+  }
+
+  res.send("authenticated successfully")
+});
+
+app.listen(port, () => {
+  console.log(`Server running at port ${port}`);
+});
