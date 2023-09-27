3cda9082e72df220b0a450fbd627be48aacf736f
Alan <zeyu.weng@gmail.com>
2023-09-26 20:44:20 -0400

    messing around w cookies

--- app/app.js
+++ app/app.js
@@ -1,1 +1,8 @@
-console.log("Hello!");
+const testFetch = (async ()=> {
+    const test = await fetch("http://localhost:3000/api")
+    const data = await test.json()
+    console.log(data)
+})
+
+testFetch()
+
--- server/index.js
+++ server/index.js
@@ -1,7 +1,8 @@
-const test = require('dotenv').config()
+const test = require("dotenv").config();
 const express = require("express");
 const querystring = require("node:querystring");
 const crypto = require("crypto");
+const cookieParser = require("cookie-parser");
 
 const port = 3000;
 const CLIENT_ID = process.env.SPOTIFY_CLIENT_ID;
@@ -9,9 +10,15 @@ const CLIENT_SECRET = process.env.SPOTIFY_CLIENT_SECRET;
 const redirect_uri = "http://localhost:3000/callback";
 
 const app = express();
+//look into this more, it no longer logs out sessionID if you disable app folder?
+// app.use(express.static("app"));
+app.use(cookieParser(`${process.env.COOKIE_SECRET}`));
 
 app.get("/", (req, res) => {
-  res.send("Hello world!");
+  res.cookie("access_token", "random access", { signed: true });
+  let test = req.signedCookies;
+  console.log(test);
+  res.send("hello world");
 });
 
 app.get("/login", (req, res) => {
@@ -19,13 +26,13 @@ app.get("/login", (req, res) => {
   let scope = "user-read-private user-read-email";
   res.redirect(
     "https://accounts.spotify.com/authorize?" +
-    querystring.stringify({
-      response_type: "code",
-      client_id: CLIENT_ID,
-      scope: scope,
-      redirect_uri: redirect_uri,
-      state: state,
-    })
+      querystring.stringify({
+        response_type: "code",
+        client_id: CLIENT_ID,
+        scope: scope,
+        redirect_uri: redirect_uri,
+        state: state,
+      })
   );
 });
 
@@ -40,7 +47,7 @@ app.get("/callback", async (req, res) => {
       grant_type: "authorization_code",
     },
     headers: {
-      "Authorization":
+      Authorization:
         "Basic " +
         new Buffer.from(CLIENT_ID + ":" + CLIENT_SECRET).toString("base64"),
       "Content-Type": "application/x-www-form-urlencoded",
@@ -48,23 +55,30 @@ app.get("/callback", async (req, res) => {
     json: true,
   };
 
-
-  const response = await fetch(authOptions.url, { headers: authOptions.headers, method: "POST", body: new URLSearchParams(authOptions.form) })
-  const result = await response.json()
-  const access_token = result.access_token
-  const refresh_token = result.refresh_token
+  const response = await fetch(authOptions.url, {
+    headers: authOptions.headers,
+    method: "POST",
+    body: new URLSearchParams(authOptions.form),
+  });
+  const result = await response.json();
+  const access_token = result.access_token;
+  const refresh_token = result.refresh_token;
 
   if (access_token) {
     const data = await fetch("https://api.spotify.com/v1/me", {
       headers: {
         Authorization: `Bearer ${access_token}`,
-      }
-    })
-    const profile = await data.json()
-    console.log(profile)
+      },
+    });
+    const profile = await data.json();
+    console.log(profile);
   }
 
-  res.send("authenticated successfully")
+  res.send("authenticated successfully");
+});
+
+app.get("/api", (req, res) => {
+  res.send({ data: "my api info" });
 });
 
 app.listen(port, () => {
